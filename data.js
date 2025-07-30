44d3e1f8fa86be8070243a338e41d2836d8e13a9
nazzmulhq <nazmul2018s@gmail.com>
2025-07-30 16:08:16 +0600

    sfdf

---
+++ data.js
@@ -0,0 +1,236 @@
+const data = [
+	{
+		page_name: "Tashrif-تشريف",
+		page_link: "https://www.facebook.com/Tashrifbd2/",
+		web: "https://tashrifbd.com/",
+		products: [
+			{
+				name: "Ahmed Al Maghribi Kaaf – Unisex (Decant)",
+				"15ml": "700",
+				"30ml": "1350",
+			},
+			{
+				name: "Allure Homme Sport",
+				"15ml": "940",
+				"30ml": "1350",
+				"100ml": "3550",
+			},
+			{
+				name: "Aqua Di Gio Profumo",
+				"15ml": "940",
+				"30ml": "1350",
+				"100ml": "3550",
+			},
+			{
+				name: "Armani Code",
+				"15ml": "940",
+				"30ml": "1350",
+				"100ml": "3550",
+			},
+			{
+				name: "Azure Royal - Unisex (Decant)",
+				"15ml": "750",
+				"30ml": "1350",
+			},
+			{
+				name: "Bleu De Chanel",
+				"15ml": "940",
+				"30ml": "1350",
+				"100ml": "3550",
+			},
+			{
+				name: "Blue by AHMED",
+				"15ml": "650",
+				"30ml": "1200",
+				"100ml": "2300",
+			},
+			{
+				name: "City Walk",
+				"15ml": "940",
+				"30ml": "1350",
+				"100ml": "3550",
+			},
+			{
+				name: "Creed Aventus",
+				price_range: "1,060৳ through 4,500৳",
+				"15ml": "1060",
+				"30ml": "2000",
+				"100ml": "4500",
+			},
+			{
+				name: "Dior Homme Intense",
+				"15ml": "940",
+				"30ml": "1350",
+				"100ml": "3550",
+			},
+			{
+				name: "Dior Sauvage",
+				"15ml": "940",
+				"30ml": "1350",
+				"100ml": "3550",
+			},
+			{
+				name: "Good Girl",
+				"15ml": "940",
+				"30ml": "1350",
+				"100ml": "3550",
+			},
+			{
+				name: "Greenley Parfums de Marly",
+				"15ml": "1060",
+				"30ml": "2000",
+				"100ml": "4500",
+			},
+			{
+				name: "Hawas Ice",
+				"15ml": "800",
+				"30ml": "1500",
+			},
+			{
+				name: "Jeleny by Ahmed al Magribi",
+				"15ml": "600",
+				"30ml": "1150",
+				"100ml": "2350",
+			},
+			{
+				name: "L Immensite Louis Vuitton",
+				price_range: "1,230৳ through 5,500৳",
+				"15ml": "1230",
+				"30ml": "2300",
+				"100ml": "5500",
+			},
+			{
+				name: "Legendary",
+				"15ml": "940",
+				"30ml": "1600",
+				"100ml": "3550",
+			},
+			{
+				name: "LV Imagination",
+				"15ml": "1230",
+				"30ml": "2300",
+				"100ml": "5600",
+			},
+			{
+				name: "Oud Wood",
+				"15ml": "1000",
+				"30ml": "1900",
+				"100ml": "4200",
+			},
+			{
+				name: "Pour Homme",
+				price_range: "1,100৳ through 4,440৳",
+				"15ml": "1100",
+				"30ml": "2000",
+				"100ml": "4440",
+			},
+			{
+				name: "Qissa Blue Paris Corner",
+				"15ml": "700",
+				"30ml": "1200",
+				"100ml": "2400",
+			},
+			{
+				name: "SRK Signature",
+				price_range: "550৳ through 5,600৳",
+				"15ml": "1250",
+				"30ml": "2200",
+				"100ml": "5600",
+			},
+			{
+				name: "YSL Y",
+				price_range: "940৳ through 3,550৳",
+				"15ml": "940",
+				"30ml": "1600",
+				"100ml": "3550",
+			},
+		],
+	},
+	{
+		page_name: "Taza Scent",
+		page_link: "https://www.facebook.com/61556335596874/",
+	},
+	{
+		page_name: "TeraBot: AI Chatbot Assistant",
+		page_link: "https://www.facebook.com/61576152824897/",
+	},
+	{
+		page_name: "The Heaven Perfume",
+		page_link: "https://www.facebook.com/THEHEAVENPERFUME/",
+	},
+	{
+		page_name: "TOHFA ZONE",
+		page_link: "https://www.facebook.com/tohfazone/",
+	},
+	{
+		page_name: "Velvet Novels",
+		page_link: "https://www.facebook.com/61574027323953/",
+	},
+	{
+		page_name: "Villa De Fragrance BD",
+		page_link: "https://www.facebook.com/61552226332549/",
+	},
+	{
+		page_name: "Vincent Fragrance",
+		page_link: "https://www.facebook.com/vincentfragrance/",
+	},
+	{
+		page_name: "Voyd Men",
+		page_link: "https://www.facebook.com/voydmen/",
+	},
+	{
+		page_name: "watch wallet attar sunglasses",
+		page_link: "https://www.facebook.com/61571129511585/",
+	},
+	{
+		page_name: "Wonderful SHORT TV",
+		page_link: "https://www.facebook.com/61572800456157/",
+	},
+	{
+		page_name: "Yee young",
+		page_link: "https://www.facebook.com/61566779197801/",
+	},
+	{
+		page_name: "ZafranX",
+		page_link: "https://www.facebook.com/zafranx/",
+	},
+	{
+		page_name: "ZAYAN",
+		page_link: "https://www.facebook.com/zayanpf/",
+	},
+	{
+		page_name: "Be king",
+		page_link: "https://www.facebook.com/profile.php?id=100063505659206",
+		web: "",
+	},
+	{
+		page_name: "RAS Perfume",
+		page_link: "https://www.facebook.com/ras.perfum.buy/",
+		web: "",
+	},
+	{
+		page_name: "VERUS",
+		page_link: "https://www.facebook.com/profile.php?id=100064273293422",
+		web: "",
+	},
+	{
+		page_name: "Marbel.store",
+		page_link: "https://www.facebook.com/marbel.store.bd/",
+		web: "https://marbel.store/",
+	},
+	{
+		page_name: "Suroor Fragrances",
+		page_link: "https://www.facebook.com/profile.php?id=61559204188960",
+		web: "",
+	},
+	{
+		page_name: "tashrifbd",
+		page_link: "https://www.facebook.com/Tashrifbd2",
+		web: "https://tashrifbd.com/",
+	},
+	{
+		page_name: "Aroha Fragnance",
+		page_link: "https://www.facebook.com/arohafragrance/",
+		web: "https://arohafragrance.sellbajar.com/",
+	},
+];
